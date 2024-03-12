650cac650b413b63215d6e4ede64220366b3d6c8
Ericblak <robiowolf1@gmail.com>
2024-03-12 16:35:28 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num in range(0, 10):
+    for numb in range(num + 1, 10):
+        if num == 8 and numb == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(num, numb), end='')
